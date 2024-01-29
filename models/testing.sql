47f3e7f788bb3224743a2c540f27b7c7a3e37b78
root <root@ip-10-0-2-152.ec2.internal>
2024-01-29 14:12:29 +0000

    sdfs

--- models/testing.sql
+++ models/testing.sql
@@ -27,12 +27,12 @@ WITH detailed_orders AS (
             WHEN l.l_discount > 0 AND l.l_extendedprice > 10000 THEN 'High Value Discounted'
             ELSE 'Regular'
         END as order_category
-    FROM "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF100".orders o
-    JOIN "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF100".lineitem l ON o.o_orderkey = l.l_orderkey
-    JOIN "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF100".customer c ON o.o_custkey = c.c_custkey
-    JOIN "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF100".nation n ON c.c_nationkey = n.n_nationkey
-    JOIN "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF100".supplier s ON l.l_suppkey = s.s_suppkey
-    JOIN "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF100".nation sn ON s.s_nationkey = sn.n_nationkey
+    FROM "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF10".orders o
+    JOIN "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF10".lineitem l ON o.o_orderkey = l.l_orderkey
+    JOIN "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF10".customer c ON o.o_custkey = c.c_custkey
+    JOIN "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF10".nation n ON c.c_nationkey = n.n_nationkey
+    JOIN "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF10".supplier s ON l.l_suppkey = s.s_suppkey
+    JOIN "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF10".nation sn ON s.s_nationkey = sn.n_nationkey
 )
 SELECT 
     *,
